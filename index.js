d221f817cdb520d19381ac8b0a359d559106c604
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-01-05 04:40:27 +0200

    initial release

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const isArray = Array.isArray
+
+module.exports = value =>
+{
+    if (typeof value == "string")
+    {
+        try { value = JSON.parse(value) }
+        catch (error) { value = [ ...value ] }
+    }
+
+    if (value == null)
+        return new Map
+
+    if (value instanceof Map)
+        return value
+
+    if (value[Symbol.toStringTag] == "Map")
+        return value
+
+    if (isArray(value) && value.every(isArray))
+        return new Map(value)
+
+    if (typeof value.entries == "function")
+        return new Map(value.entries())
+
+    if (typeof value[Symbol.iterator] == "function")
+        return new Map(value)
+
+    return new Map(Object.entries(value))
+}
