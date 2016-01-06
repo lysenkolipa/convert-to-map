66da56910b95872ebc5acfa06f7bfe0717ecd315
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-01-06 02:11:33 +0200

    made script browser friendly

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-"use strict"
-
-const isArray = Array.isArray
-
-module.exports = value =>
+(function()
 {
-    if (typeof value == "string")
+    "use strict"
+
+    var isArray = Array.isArray
+
+    module.exports = function(value)
     {
-        try { value = JSON.parse(value) }
-        catch (error) { value = [ ...value ] }
+        if (typeof value == "string")
+        {
+            try { value = JSON.parse(value) }
+            catch (error) { value = [ ...value ] }
+        }
+
+        if (value == null)
+            return new Map
+
+        if (value instanceof Map)
+            return value
+
+        if (value[Symbol.toStringTag] == "Map")
+            return value
+
+        if (isArray(value) && value.every(isArray))
+            return new Map(value)
+
+        if (typeof value.entries == "function")
+            return new Map(value.entries())
+
+        if (typeof value[Symbol.iterator] == "function")
+            return new Map(value)
+
+        return new Map(Object.entries(value))
     }
-
-    if (value == null)
-        return new Map
-
-    if (value instanceof Map)
-        return value
-
-    if (value[Symbol.toStringTag] == "Map")
-        return value
-
-    if (isArray(value) && value.every(isArray))
-        return new Map(value)
-
-    if (typeof value.entries == "function")
-        return new Map(value.entries())
-
-    if (typeof value[Symbol.iterator] == "function")
-        return new Map(value)
-
-    return new Map(Object.entries(value))
-}
+})()
